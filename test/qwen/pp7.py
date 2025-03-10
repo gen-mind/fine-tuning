bfc1ce6637eabdc51d7819c73c0631f47a0aab0c
Gian Paolo Santopaolo <gianpaolo.santopaolo@gmail.com>
2025-03-10 01:18:50 +0100

    Update pp7.py

--- test/qwen/pp7.py
+++ test/qwen/pp7.py
@@ -200,7 +200,7 @@ def main():
         # eval_dataset=eval_data,
         args=TrainingArguments(
             num_train_epochs=1,
-            per_device_eval_batch_size=2,
+            per_device_eval_batch_size=1,
             per_device_train_batch_size=4,
             gradient_accumulation_steps=4,
             learning_rate=1e-4,
