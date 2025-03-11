ac3a110185503da487a774e33ab493fe11748ad9
Gian Paolo Santopaolo <gianpaolo.santopaolo@gmail.com>
2025-03-11 23:10:04 +0100

    fix

--- test/qwen/pp11.py
+++ test/qwen/pp11.py
@@ -199,7 +199,7 @@ def main():
         train_dataset=tokenized_dataset,  #train_data,
         # eval_dataset=eval_data,
         args=TrainingArguments(
-            num_train_epochs=1,
+            num_train_epochs=2,
             per_device_train_batch_size=4,
             # per_device_eval_batch_size=2,
             gradient_accumulation_steps=4,
